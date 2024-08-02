f3fde05b46a1aa878544ddab3c9722c3f6185fde
John Marquez <johnmarquez249@gmail.com>
2024-08-01 19:03:53 -0600

    fixed deployment

--- assets/scripts/quiz.js
+++ assets/scripts/quiz.js
@@ -1,75 +1,52 @@
 const btnStart = document.querySelector("#btn-start-quiz");
 const btnStartModal = document.querySelector("#btn-start-quiz-modal");
 
-const renderQuiz = async function( ) {
+const renderQuiz = async function () {
+  const quizError = document.querySelector("#quiz-error");
+  const quizInfo = document.querySelector("#quiz-info");
+  quizId = parseInt(localStorage.getItem("quizeId"));
 
-    const quizError = document.querySelector("#quiz-error");
-    const quizInfo = document.querySelector("#quiz-info");
-    quizId = parseInt( localStorage.getItem("quizeId") );
+  if (isNaN(quizId)) {
+    quizInfo.classList.add("d-none");
+    quizError.classList.remove("d-none");
+  } else {
+    quizInfo.classList.remove("d-none");
+    quizError.classList.add("d-none");
 
-    if( isNaN( quizId ) ) {
+    const quiz = await fetchQuizData(quizId);
 
-        quizInfo.classList.add("d-none");
-        quizError.classList.remove("d-none");
-       
-    } else {
+    const quizName = document.querySelector("#quiz-name");
+    const quizDesc = document.querySelector("#quiz-desc");
+    const totalCount = document.querySelector("#quiz-ques");
 
-        quizInfo.classList.remove("d-none");
-        quizError.classList.add("d-none");
+    quizName.textContent = quiz.quizName;
+    quizDesc.textContent = quiz.quizDesc;
+    totalCount.textContent = quiz.questions.length;
+  }
+};
 
-        const quiz = await fetchQuizData(quizId);
+btnStart.addEventListener("click", function () {
+  var myModal = new bootstrap.Modal(document.getElementById("modal-username"));
+  document.querySelector("#username-error").classList.add("d-none");
+  myModal.show();
+});
 
-        const quizName = document.querySelector("#quiz-name");
-        const quizDesc = document.querySelector("#quiz-desc");
-        const totalCount = document.querySelector("#quiz-ques");
+btnStartModal.addEventListener("click", function () {
+  const usernameError = document.querySelector("#username-error");
+  const username = document.querySelector("#username").value.trim();
 
-        quizName.textContent = quiz.quizName;
-        quizDesc.textContent = quiz.quizDesc;
-        totalCount.textContent = quiz.questions.length;
+  if (username === "") {
+    usernameError.textContent = "Enter username to start quiz.";
+    usernameError.classList.remove("d-none");
+  } else {
+    usernameError.classList.add("d-none");
+    localStorage.setItem("username", username);
 
-    }
-    
+    let url = '../question.html';
+
+    console.log(`Navigating to URL: ${url}`);
+    location.assign(url);
 }
-
-
-btnStart.addEventListener('click',function(){
-    
-    var myModal = new bootstrap.Modal(document.getElementById('modal-username'));
-    document.querySelector("#username-error").classList.add('d-none');
-    myModal.show();
-
-
 });
-
-
-
-btnStartModal.addEventListener('click',function(){
-
-    const usernameError = document.querySelector("#username-error");
-    const username = document.querySelector("#username").value.trim();
-    
-    if( username === "" ) {
-
-        usernameError.textContent = 'Enter username to start quiz.';
-        usernameError.classList.remove('d-none');
-    } else {
-        usernameError.classList.add('d-none');
-        localStorage.setItem("username",username);
-        
-        let url = '';
-        if( window.location.pathname === '/Code-Quiz/' ) {
-
-            url = window.location.hostname+window.location.pathname+"question.html";
-        } else {
-        
-            url = window.location.origin+"/question.html";
-        }
-        
-        location.assign(url);
-    }
-
-});
-
 
 renderQuiz();
-
